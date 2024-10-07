b4b842ef5388832ea6ea0c9607bb52c2b43e2057
xfo79k <xfo79k@gmail.com>
2024-10-08 00:07:27 +0800

    feat: init

--- dist/postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {},
-    }
-};
-//# sourceMappingURL=postcss.config.js.map
--- dist/vite.preload.config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vite_1 = require("vite");
-// https://vitejs.dev/config
-exports.default = (0, vite_1.defineConfig)({});
-//# sourceMappingURL=vite.preload.config.js.map
